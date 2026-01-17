6e129601466748995695fb07db40611214307157
Mouaz Jamal <mj225ih@student.lnu.se>
2026-01-17 21:39:01 +0100

    Add key and url for api

---
+++ index.js
@@ -0,0 +1,2 @@
+const apiKey = '362aa7ded0637aff3ba48a23913511f5'
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm&appid=362aa7ded0637aff3ba48a23913511f5&units=metric&lang=se'
