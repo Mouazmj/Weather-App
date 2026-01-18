fa77f9459d734e0a482226416244fecbe37e9340
Mouaz Jamal <mj225ih@student.lnu.se>
2026-01-18 18:46:19 +0100

    Added switch to be able to load the img for the weather type

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q='
 
 const button = document.querySelector('.search button')
 const input = document.querySelector('.search input')
+const icon = document.querySelector('.weather-icon')
 
 
 async function checkWether (city) {
@@ -15,6 +16,27 @@ async function checkWether (city) {
    document.querySelector('.city').innerHTML = data.name
    document.querySelector('.humidity').innerHTML = data.main.humidity + '%'
    document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h'
+
+   switch (data.weather[0].main) {
+    case 'Clouds':
+        icon.src = './images/clouds.png';
+        break;
+        case 'Clear':
+            icon.src = './images/clear.png';
+            break;
+            case 'Drizzle':
+                icon.src = './images/drizzle.png';
+                break;
+                case 'Mist':
+                    icon.src = './images/mist.png';
+                    break;
+                    case 'Rain':
+                        icon.src = './images/rain.png';
+                        break;
+                        case 'Snow':
+                        icon.src = './images/snow.png';
+                        break;
+   }
 }
 
 button.addEventListener('click', () => {
